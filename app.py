81b646c22759e107f131a52e48895e2f9b4429be
Hitanshi <hitanshi.jadav07@gmail.com>
2026-01-17 13:45:17 +0530

    added ci/cd

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello. This app is deployed using DevOps pipeline."
+    return "Hello. This app is deployed using DevOps pipeline. \n" \
+    "Added docker.yml and now checking if image appears in dockerhub"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
