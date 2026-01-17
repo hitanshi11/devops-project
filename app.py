cbad11b50d109b081cb47937afdd556e13fe2c2e
Hitanshi <hitanshi.jadav07@gmail.com>
2026-01-17 13:35:30 +0530

    docker yaml file and app changes

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello...... This app is deployed using DevOps pipeline."
+    return "Hello. This app is deployed using DevOps pipeline."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
